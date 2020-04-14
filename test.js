ca2287263f7a284d6083d9089d68958611e9b0a2
pierman1 <pierreb.webdesign@gmail.com>
2020-04-14 17:56:34 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
 alert('initiated');
 
-var ctaText = ''
+var bannerObject = {
+  text: '',
+  postion: ''
+}
+
+function parseQuery(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return results[2].replace(/\+/g, ' ')
+}
 
 function tagInit(name, url) {
   if (!url) url = window.location.href;
@@ -9,7 +22,6 @@ function tagInit(name, url) {
     results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
-  ctaText = results[2].replace(/\+/g, ' ')
   return true
 }
 
@@ -18,9 +30,9 @@ function myFunction() {
 
   Array.from(scripts).forEach(function(script) {
      if (tagInit('text', script.src)) {
-       console.log('!! script detected !!!', {
-         ctaText
-       })
+       bannerObject.text = tagInit('text', script.src)
+       bannerObject.position = tagInit('position', script.src)
+       console.log('bannerObject', bannerObject)
      } 
   });
 }
