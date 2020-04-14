b891e85cabbd4be09b4d588b530fcffa6e09c1bb
pierman1 <pierreb.webdesign@gmail.com>
2020-04-14 15:10:20 +0200

    Update test.js

--- test.js
+++ test.js
@@ -14,9 +14,6 @@ function getParameterByName(name, url) {
 function myFunction() {
   var scripts = document.getElementsByTagName("script")
   console.log('ssss', scripts)
-//   scripts.forEach((script) => {
-//     console.log('script', script)
-//   })
 }
 
 setInterval(function () {
